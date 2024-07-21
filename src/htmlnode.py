e2c62d618644f5b0b4e27b73f2000b77fb579cc4
Mehki Joseph <disismehki@gmail.com>
2024-07-21 14:30:54 -0400

    Create LeafNode and fix bug in HTMLNode
    Fixed issue where if self.props was None, the props_to_html method returned an AttributeError; removed returning print statement in props_to_html method

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,10 +11,30 @@ class HTMLNode:
     def props_to_html(self):
         text = ""
 
-        for key, value in self.props.items():
-            text += f' {key}="{value}"'
+        if self.props is not None:
+            for key, value in self.props.items():
+                text += f' {key}="{value}"'
+        else:
+            text = ""
         
-        return print(text)
+        return text
 
     def __repr__(self):
         return f"Tag: {self.tag}, Value: {self.value}, Children: {self.children}, Props: {self.props}"
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, props=None):
+        super().__init__(tag, props)
+        self.value = value
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All leaf nodes must have a value.")
+        
+        if not self.tag:
+            return f"{self.value}"
+        
+        if self.tag == "a":
+            return f"<{self.tag}{self.props_to_html()} {self.value}/>"
+        
+        return f"<{self.tag}>{self.value}</{self.tag}>"
