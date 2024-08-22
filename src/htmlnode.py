b91417f3df8ccd5e1f4f096605a1495ce593412b
Mehki Joseph <disismehki@gmail.com>
2024-08-22 13:17:36 -0400

    Fix positional arguments in ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -37,7 +37,7 @@ class LeafNode(HTMLNode):
 
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
-        super().__init__(tag, props, children)
+        super().__init__(tag, None, children, props)
     
     def to_html(self):
         if not self.tag:
