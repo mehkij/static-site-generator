66b0fd75ebad40bbd1e6bdb3911419d46b61e201
Mehki Joseph <disismehki@gmail.com>
2024-08-22 12:14:09 -0400

    Create ParentNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -35,3 +35,19 @@ class LeafNode(HTMLNode):
             return f"{self.value}"
         
         return f"<{self.tag}>{self.props_to_html()}{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, props, children)
+    
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("All elements must have a tag.")
+        
+        if not self.children:
+            raise ValueError("All parent nodes must have children.")
+
+        # Recursive case
+        children_html = "".join(child.to_html() for child in self.children)
+
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
