7ba9e23df685e4064e3962d05f1f87db5338bdc7
Mehki Joseph <disismehki@gmail.com>
2024-08-22 22:51:24 -0400

    Move text_node_to_html_node into textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode
-from text_node_to_html_node import text_node_to_html_node
+from textnode import text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
